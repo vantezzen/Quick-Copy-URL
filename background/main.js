c38b2541e78b11691ba07b868597f125671f28a1
vantezzen <hello@vantezzen.io>
2020-04-12 11:53:40 +0200

    Add support for shortcuts

--- background/main.js
+++ background/main.js
@@ -1,19 +1,51 @@
-chrome.runtime.onMessage.addListener(
-  (type) => {
+const setIcon = (type) => {
+  chrome.browserAction.setIcon({
+    path: {
+      32: `../icons/${type}-32.png`,
+      48: `../icons/${type}-48.png`,
+    }
+  });
+
+  setTimeout(() => {
     chrome.browserAction.setIcon({
       path: {
-        32: `../icons/${type}-32.png`,
-        48: `../icons/${type}-48.png`,
+        32: '../icons/icon-32.png',
+        48: '../icons/icon-48.png',
       }
     });
+  }, 500);
+};
 
-    setTimeout(() => {
-      chrome.browserAction.setIcon({
-        path: {
-          32: '../icons/icon-32.png',
-          48: '../icons/icon-48.png',
+const copyUrlToClipboard = () => {
+  // Get current page url
+  try {
+    chrome.tabs.query({currentWindow: true, active: true}, tabs => {
+      console.log('TABS', tabs);
+      chrome.tabs.sendMessage(tabs[0].id, 'copy', (type) => {
+        if (type) {
+          setIcon(type);
         }
       });
-    }, 500);
+    });
+  } catch (e) {
+    console.debug('Could not copy URL', e);
+    setIcon('failed');
+  }
+}
+
+chrome.runtime.onMessage.addListener(
+  (type) => {
+    if (type === "copy") {
+      copyUrlToClipboard();
+    } else {
+      setIcon(type);
+    }
   }
 );
+
+chrome.commands.onCommand.addListener(function (command) {
+  if (command === "copy-url") {
+    console.log("Received shortcut");
+    copyUrlToClipboard();
+  }
+});
--- popup/main.js
+++ popup/main.js
@@ -6,43 +6,5 @@
  * @link        https://github.com/vantezzen/quick-copy-url
  * @license     https://opensource.org/licenses/mit-license.php MIT License
  */
-const copyUrlToClipboard = () => {
-  // Get current page url
-  chrome.tabs.query({currentWindow: true, active: true}, tabs => {
-    // Get domain of current tab
-    const url = tabs[0].url;
-    
-    // Copy URL to clipboard
-    navigator.clipboard.writeText(url)
-      .then(() => {
-        chrome.runtime.sendMessage('success');
-        console.debug('Text copied to clipboard');
-
-        window.close();
-      })
-      .catch(err => {
-        chrome.runtime.sendMessage('failed');
-        console.debug('Could not copy text: ', err);
-
-        window.close();
-      });
-  });
-}
-
-if (navigator.userAgent.toLowerCase().indexOf('firefox') > -1) {
-  // Firefox doesn't have clipboard-write permission - directly try to copy
-  copyUrlToClipboard();
-} else {
-  // Get clipboard write permissions
-  navigator.permissions.query({name: "clipboard-write"}).then(result => {
-    if (result.state == "granted" || result.state == "prompt") {
-      copyUrlToClipboard();
-    } else {
-      chrome.runtime.sendMessage('failed');
-      // User has not granted access to the clipboard - abort
-      console.debug('Cannot use clipboard');
-  
-      window.close();
-    }
-  });
-}
+chrome.runtime.sendMessage('copy');
+window.close();
